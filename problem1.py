49d9a07116d4498b175c8d8e21b97b813331a769
Nabdeep <nabkaurdeep2002@gmail.com>
2023-03-31 07:16:23 -0700

    v2

--- problem1.py
+++ problem1.py
@@ -13,9 +13,25 @@ if c==y:
     e =input("Was X-Ray performed? (y/n):")
     if e==y:
       print("X-Ray rate: $100")
-      t =input("total bill:")
-      #calculatinf tax
+      
+      #calculate tax
+      total =float(input("total bill:"))
       tax_rate=0.15
     tax =total*tax_rate
-    total =total+tax
-    print("your total taxes are: ",tax)
+    bill =total+tax
+    print(bill)
+    #for discount
+        #discount
+    total =float(input("total bill:"))
+    disc1 =0.05
+    disc2 =0.1
+    if(total>300):
+      disc = total * disc2
+      total_amount2 =total-disc
+      print(total_amount2)
+    elif(total>200):
+      disc = total * disc1
+      total_amount1 =total-disc
+      print(total_amount1)
+    elif (total<200):
+      print(total)
